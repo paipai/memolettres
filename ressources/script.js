2aeeab29af6bbd64ed924bbf4af126056f9b8589
Pigeard Sylvain <13221271+PigeardSylvain@users.noreply.github.com>
2020-08-18 12:48:58 +0200

    update service url

--- ressources/script.js
+++ ressources/script.js
@@ -37,7 +37,7 @@ function initWorker() {
     if ('serviceWorker' in navigator) {
         let registration;
         const registerServiceWorker = async () => {
-            registration = await navigator.serviceWorker.register('../service-worker.js');
+            registration = await navigator.serviceWorker.register('https://paipai.github.io/memolettres/service-worker.js');
         };
         registerServiceWorker();
     }
